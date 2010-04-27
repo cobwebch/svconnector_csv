5cbde5f876bfff2066b2105fca9cbc1410ecd9d5
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-27 16:04:33 +0000

    Released version 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_csv/trunk@32653 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "svconnector_csv"
 #
-# Auto generated 18-01-2009 17:46
+# Auto generated 27-04-2010 18:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'svconnector' => '1.1.0-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"a0fb";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"c460";s:17:"ext_localconf.php";s:4:"8c7f";s:14:"doc/manual.sxw";s:4:"3f74";s:35:"sv1/class.tx_svconnectorcsv_sv1.php";s:4:"e33a";s:17:"sv1/locallang.xml";s:4:"e842";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"a0fb";s:10:"README.txt";s:4:"dfb5";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"c460";s:17:"ext_localconf.php";s:4:"8c7f";s:14:"doc/manual.sxw";s:4:"9ce9";s:35:"sv1/class.tx_svconnectorcsv_sv1.php";s:4:"6aa5";s:17:"sv1/locallang.xml";s:4:"e842";}',
 	'suggests' => array(
 	),
 );
