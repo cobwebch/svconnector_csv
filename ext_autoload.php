1174598318c66662b2fcdbe64aebd0024261678e
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-20 21:43:13 +0000

    Added autoloader file
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_csv/trunk@40484 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_autoload.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * Register necessary class names with autoloader
+ *
+ * $Id: ext_autoload.php 26472 2009-11-13 10:48:15Z francois $
+ */
+return array(
+	'tx_svconnectorcsv_sv1' => t3lib_extMgm::extPath('svconnector_csv', 'sv1/class.tx_svconnectorcsv_sv1.php'),
+);
+?>
