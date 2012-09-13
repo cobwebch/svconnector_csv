388a75bcca4875a97e03d7f33aca737a2139e8d2
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-13 12:21:38 +0000

    CGL cleanups
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_csv/trunk@66000 735d13b6-9817-0410-8766-e36946ffe9aa

--- sv1/class.tx_svconnectorcsv_sv1.php
+++ sv1/class.tx_svconnectorcsv_sv1.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2008-2010 Francois Suter (Cobweb) <typo3@cobweb.ch>
+*  (c) 2008-2012 Francois Suter (Cobweb) <typo3@cobweb.ch>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -44,7 +44,7 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	 * In the case of CSV, it is always the case
 	 * It might fail for a specific file, but it is always available in general
 	 *
-	 * @return	boolean		TRUE if the service is available
+	 * @return boolean TRUE if the service is available
 	 */
 	public function init() {
 		parent::init();
@@ -57,9 +57,9 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	 * This method calls the query method and returns the result as is,
 	 * i.e. the parsed CSV data, but without any additional work performed on it
 	 *
-	 * @param	array	$parameters: parameters for the call
+	 * @param array $parameters Parameters for the call
 	 *
-	 * @return	mixed	server response
+	 * @return mixed Server response
 	 */
 	public function fetchRaw($parameters) {
 		$result = $this->query($parameters);
@@ -76,9 +76,9 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	/**
 	 * This method calls the query and returns the results from the response as an XML structure
 	 *
-	 * @param	array	$parameters: parameters for the call
+	 * @param array $parameters Parameters for the call
 	 *
-	 * @return	string	XML structure
+	 * @return string XML structure
 	 */
 	public function fetchXML($parameters) {
 			// Get the data as an array
@@ -98,9 +98,9 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	/**
 	 * This method calls the query and returns the results from the response as a PHP array
 	 *
-	 * @param	array	$parameters: parameters for the call
+	 * @param array $parameters Parameters for the call
 	 *
-	 * @return	array	PHP array
+	 * @return array PHP array
 	 */
 	public function fetchArray($parameters) {
 		$headers = array();
@@ -147,11 +147,12 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	 * This method reads the content of the file defined in the parameters
 	 * and returns it as a single string
 	 *
-	 * NOTE:	this method does not implement the "processParameters" hook,
-	 *			as it does not make sense in this case
+	 * NOTE: this method does not implement the "processParameters" hook,
+	 *       as it does not make sense in this case
 	 *
-	 * @param	array	$parameters: parameters for the call
-	 * @return	string	content of the file
+	 * @param array $parameters Parameters for the call
+	 * @throws Exception
+	 * @return string Content of the file
 	 */
 	protected function query($parameters) {
 		$fileData = array();
