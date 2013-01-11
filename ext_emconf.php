1f13cb9a117eadd48f53d8f036acbf312bb3dec4
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-11 19:27:17 +0000

    Verified compatibility with TYPO3 CMS 6.0, resolves #44464
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_csv/trunk@69824 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
-			'svconnector' => '2.0.0-0.0.0',
+			'typo3' => '4.5.0-6.0.99',
+			'svconnector' => '2.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
