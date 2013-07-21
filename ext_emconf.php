ef9864a4a88b3d2b7ce5664b36f9681fec53d322
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-21 19:59:38 +0000

    Verified compatibility with TYPO3 CMS 6.1, resolves #50257 Removed unnecessary require statement
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_csv/trunk@78108 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.0.99',
+			'typo3' => '4.5.0-6.1.99',
 			'svconnector' => '2.2.0-0.0.0',
 		),
 		'conflicts' => array(
