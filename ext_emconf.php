cba4e794954a2331707788d16fa6e5895522db1d
Francois Suter <francois@typo3.org>
2014-04-09 17:11:32 +0200

    [TASK] Verify compatibility with TYPO3 6.2
    Ensure compatibility with TYPO3 CMS from version 4.5 to 6.2.
    
    Resolves: #57766
    Releases: 1.5
    Change-Id: Icd5071cf7b17d52b51e77b8c6f6e00521187d26d
    Reviewed-on: https://review.typo3.org/29319
    Reviewed-by: Francois Suter
    Tested-by: Francois Suter

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '4.5.0-6.1.99',
+      'typo3' => '4.5.0-6.2.99',
       'svconnector' => '2.4.0-0.0.0',
     ),
     'conflicts' =>
